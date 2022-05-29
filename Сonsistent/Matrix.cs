2cc46d8d787afba2d4493cebaa49d20067e7b468
AntonShchur <54263356+AntonShchur@users.noreply.github.com>
2022-05-29 15:25:25 +0300

    Add files via upload

---
+++ Сonsistent/Matrix.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Сonsistent
+{
+    class Matrix
+    {
+        private int n_rows;
+        private int n_columns;
+        private double[,] matrix;
+
+        public Matrix(int n_rows, int n_columns)
+        {
+            this.n_rows = n_rows;
+            this.n_columns = n_columns;
+            this.matrix = new double[n_rows, n_columns];
+
+            Random random = new Random();
+            for (int i = 0; i < n_rows; i++)
+            {
+                for (int j = 0; j < n_columns; j++)
+                {
+                    this.matrix[i, j] = random.Next(-100, 100);
+                }
+            }
+        }
+
+        public Matrix(string path_to_csv)
+        {  
+            int n_rows = 0;
+            int n_cols = 0;
+            List<string> numbers = new List<string>();
+            using (var reader = new System.IO.StreamReader($"{path_to_csv}"))
+            {
+
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine().Split(',');
+                    n_cols = line.Length;
+                    n_rows += 1;
+                    foreach (var number in line)
+                    {
+                        numbers.Add(number);
+                    }
+                }
+            }
+
+            this.n_rows = n_rows;
+            this.n_columns = n_cols;
+            this.matrix = new double[n_rows, n_cols];
+            for (int i = 0; i < n_rows; i++)
+            {
+                for (int j = 0; j < n_cols; j++)
+                {
+                    this.matrix[i, j] = double.Parse(numbers[i + j]);
+                }
+            }
+        }
+        public void WriteMatrixToFile(string path)
+        {
+            File.WriteAllText(path, string.Empty);
+            for(int i=0; i < n_rows; i++)
+            {
+                List<double> numbers = new List<double>();
+                
+                for (int j=0; j < n_columns; j++)
+                {
+                    numbers.Add(matrix[i, j]);
+                }
+                File.AppendAllText(path, string.Join(',', numbers) + "\n");
+            }
+        }
+
+        public void DrawMatrix()
+        {
+            for (int i = 0; i < n_rows; i++)
+            {
+                for (int j = 0; j < n_columns; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static Matrix MultiplyWithTest(Matrix A, Matrix B)
+        {
+            if (A.n_columns != B.n_rows)
+            {
+                throw new Exception("Для можливості множення необхідно, щоб к-сть" +
+                    " рядків першої матриці співпадала з к-стю стовпців другої");
+            };
+            Matrix C = new Matrix(A.n_rows, B.n_columns);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            C = Matrix.Multiply(A, B, C);
+            stopWatch.Stop();
+            long elapsedTime = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine("Вхідна матриця А:");
+            A.DrawMatrix();
+            Console.WriteLine("Вхідна матриця B:");
+            B.DrawMatrix();
+            Console.WriteLine("Результуюча матриця С:");
+            C.DrawMatrix();
+            Console.WriteLine($"Час, затрачений на операцію послідовного множення {elapsedTime}мс.");
+            return C;
+        }
+
+        public static Matrix Multiply(Matrix A, Matrix B, Matrix C)
+        {
+            for (int i = 0; i < A.n_rows; i++)
+            {
+
+                for (int j = 0; j < B.n_columns; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < B.n_rows; k++)
+                    {
+                        sum += A.matrix[i, k] * B.matrix[k, j];
+                    }
+                    C.matrix[i, j] = sum;
+                }
+
+            }
+            return C;
+        }
+    }
+}
---
+++ Сonsistent/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Сonsistent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("1. Переконатись в правильності роботи послідовного алгоритму множення матриць");
+            Console.WriteLine("2. Переконатись в правильності роботи послідовного алгоритму множення матриць\n" +
+                "за допомогою власних матриць з файлів .csv");
+            Console.WriteLine("3. Провести виміри часу роботи послідовного алгоритму");
+            Console.WriteLine("4. Створити файли .csv з матрицями заданих розмірностей");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        WorkTestWithRandomGen();
+                        break;
+                    }
+                case 2:
+                    {
+                        WorkTestFromFile();
+                        break;
+                    }
+                case 3:
+                    {
+                        TimeMeasuring();
+                        break;
+                    }
+                case 4:
+                    {
+                        CreateMatrixFile();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Необхідно вибрати номер зі списку");
+                        break;
+                    }
+            }
+        }
+
+        static void WorkTestWithRandomGen()
+        {
+            try
+            {
+                Console.WriteLine("Вкажіть розмірності матриці A:");
+                int m = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Вкажіть розмірності матриці B:");
+                int k = int.Parse(Console.ReadLine());
+                int i = int.Parse(Console.ReadLine());
+
+                if (m <= 0 | n <= 0 | k <= 0 | i <= 0) throw new Exception("Розмірності матриць мають бути більше 0");
+                if (n != k) throw new Exception("К-сть стовпців в першій матриці має дорівнювати к-сті рядків в другій");                Matrix matrix1 = new Matrix(m, n);
+                Matrix matrix2 = new Matrix(k, i);
+                Matrix matrix3 = Matrix.MultiplyWithTest(matrix1, matrix2);
+            }
+            catch
+            {
+                Console.WriteLine("Розмірності матриць мають бути більше 0");
+                Console.WriteLine("К-сть стовпців в першій матриці має дорівнювати к-сті рядків в другій");
+                Console.WriteLine("Перезапустіть додаток та введіть правильні числа");
+            }
+        }
+
+        static void WorkTestFromFile()
+        {
+            try
+            {
+                Console.WriteLine("Вкажіть назву файлу .csv з матицею A:");
+                string path1 = Console.ReadLine();
+                Console.WriteLine("Вкажіть назву файлу .csv з матицею В:");
+                string path2 = Console.ReadLine();
+
+                Matrix matrix1 = new Matrix($"./{path1}");
+                Matrix matrix2 = new Matrix($"./{path2}");
+                Matrix matrix3 = Matrix.MultiplyWithTest(matrix1, matrix2);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Файли не були знайдені або");
+                Console.WriteLine("К-сть стовпців в першій матриці має дорівнювати к-сті рядків в другій");
+                Console.WriteLine("Перезапустіть додаток та вкажіть правильні файли з павильними матрицями");
+            }
+
+        }
+
+        static void TimeMeasuring()
+        {
+            int startRank = 100;
+            int nIterations = 10;
+            int avgIterations = 5;
+            long[] times = new long[nIterations];
+            for(int i = 1; i <= nIterations; i++)
+            {
+                long time = 0;
+                for (int j = 0; j < avgIterations; j++)
+                {
+                    Matrix A = new Matrix(i * startRank, i * startRank);
+                    Matrix B = new Matrix(i * startRank, i * startRank);
+                    Matrix C = new Matrix(i * startRank, i * startRank);
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    C = Matrix.Multiply(A, B, C);
+                    stopwatch.Stop();
+                    time += stopwatch.ElapsedMilliseconds;
+                }
+                time /= avgIterations;
+                times[i - 1] = time;
+                Console.WriteLine($"Множення матриць розмірності {i * startRank} x {i * startRank} було завершено за {time}мс.");
+            }
+            string results = string.Join(",", times);
+            File.WriteAllText("./results.csv", results);
+        }
+
+        static void CreateMatrixFile()
+        {
+            try
+            {
+                Console.WriteLine("Вкажіть розмірності матриці A:");
+                int m = int.Parse(Console.ReadLine());
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Вкажіть розмірності матриці B:");
+                int k = int.Parse(Console.ReadLine());
+                int i = int.Parse(Console.ReadLine());
+
+                if (m <= 0 | n <= 0 | k <= 0 | i <= 0) throw new Exception("Розмірності матриць мають бути більше 0");
+
+                Matrix matrix1 = new Matrix(m, n);
+                Matrix matrix2 = new Matrix(k, i);
+                matrix1.WriteMatrixToFile("./matrix1.csv");
+                matrix2.WriteMatrixToFile("./matrix2.csv");
+                Console.WriteLine("Матриці успішно створені та записані у файл matrix1.csv, matrix2.csv");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Розмірності матриць мають бути більше 0");
+                Console.WriteLine("Перезапустіть додаток та введіть правильні числа");
+            }
+        }
+
+    }
+}
